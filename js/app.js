a6deba95f1e206730992901ddf3b8746ef4e104e
Sean.Rennie <sean.rennie6@gmail.com>
2018-07-14 07:55:51 +0100

    can now move the tank with arrow keys, uncontained within board

--- js/app.js
+++ js/app.js
@@ -1,30 +1,13 @@
 //console.log('this is linked');
-///////////////////////////////////////
-///////- COLLISION DETECTION -/////////
-///////////////////////////////////////
-
-///////- DIV MOVE AND X/Y LOG -////////
-//  log the position of a div   [X]
-//  -->save the xy of a div to a variable [X]
-//  -->determine the current position of a div x/y [log it to the console] [X]
-//  -->move the div and relog the position to the console [X]
-//----
-//  move the div on its own [X]
-//  -->continually log the xy to the console when moving
-//____________________________________
-// const ;
-
-
-
-
-///////- COMPARE DIV 'A' TO 'B' -//////////
-//  determine when one div is within another [boolean response]
-//  grab x/y from div A and B log to console and save as viables.
-//  compare position of a to position of b
-//  --> if A < B then log to console
-
-
-//  move a div across the screen by updating its x and y position [jquery? css? js?]
+////////////////////////////////////
+///////- TANK MOVE -//////////
+////////////////////////////////////
+//  make the tanks position relative [X]
+//  grab tank element from the dom [X]
+//  update the x position few pixels on up key press [X]
+//  limit the movement of the tank to its container [o]
+//  --> xy should not exceede that of the container
+//  --> this should stop the tank form moving in that direction
 
 
 ////////////////////////////////////
@@ -35,45 +18,70 @@ $(() => {
   const $bullet = $('.bullet');
   const $tank = $('.tank');
 
-  const tankPosition = {
+  function tankMove(direction) {
+    switch(direction){
+      case 'ArrowUp':
+        return moveTankUp();
+      case 'ArrowDown':
+        return moveTankDown();
+      case 'ArrowLeft':
+        return moveTankLeft();
+      case 'ArrowRight':
+        return moveTankRight();
+    }
+  }
+
+  function moveTankUp () {
+    return $tank.offset({top: $tank.offset().top - 50});
+  }
+  function moveTankDown () {
+    return $tank.offset({top: $tank.offset().top + 50});
+  }
+  function moveTankLeft () {
+    return $tank.offset({left: $tank.offset().left - 50});
+  }
+  function moveTankRight () {
+    return $tank.offset({left: $tank.offset().left + 50});
+  }
+
+  const tankOne = {
     minX: $tank.offset().left,
     minY: $tank.offset().top,
     maxX: $tank.offset().left + $tank.width(),
     maxY: $tank.offset().top + $tank.height()
   };
 
-  const bulletPosition = {
+  const bullet = {
     minX: $bullet.offset().left,
     minY: $bullet.offset().top,
     maxX: $bullet.offset().left + $bullet.width(),
-    maxY: $bullet.offset().top + $bullet.height()
+    maxY: $bullet.offset().top + $bullet.height(),
+    collisionDetected: false
   };
 
-  //prints position of boxes saved in object
-  // function printXy () {
-  //   console.log('tank minX: ' + tankPosition.minX);
-  //   console.log('tank minY: ' + tankPosition.minY);
-  //   console.log('bullet minX: ' + bulletPosition.minX);
-  //   console.log('bullet minY: ' + bulletPosition.minY);
-  // }
 
-  //to dynamically update the position of an animated element (currently not working)
+  ///////- COLLISION DETECTION -/////////
+
+  //to dynamically update the position of an animated element
+  //(works when called as step in animateBullet())
   function updatedPosition(){
-    bulletPosition.minX = $bullet.offset().left;
-    bulletPosition.minY = $bullet.offset().top;
-    bulletPosition.maxX = $bullet.offset().left + $bullet.width();
-    bulletPosition.maxY = $bullet.offset().top + $bullet.height();
+    bullet.minX = $bullet.offset().left;
+    bullet.minY = $bullet.offset().top;
+    bullet.maxX = $bullet.offset().left + $bullet.width();
+    bullet.maxY = $bullet.offset().top + $bullet.height();
     targetCollision();
   }
 
   function targetCollision () {
-    if(bulletPosition.maxX > tankPosition.minX){
-      console.log('HIT HIT HIT');
+    if(bullet.maxX > tankOne.minX){
+      bullet.collisionDetected = true;
+      console.log('HIT HIT HIT ' + bullet.collisionDetected);
     }
   }
 
   //to move bullet around screen and update its position object
   function animateBullet() {
+    console.log('I fired');
     $bullet.animate({
       left: '100%'
     }, {
@@ -82,17 +90,27 @@ $(() => {
     });
   }
 
-  function keyIdentifier(){
-    if(event.originalEvent.code === 'Space'){
-      animateBullet();
-      console.log(event);
-    }
+  //////-KEY DOWN IDENTIFIER -///////
+  //use this to determine what key has been pressed and assign correct function
+  function keyIdentifier(e){
+    console.log(e);
+    if(e.originalEvent.key === ' ') animateBullet();
+    if(e.originalEvent.key === 'ArrowDown' ||
+      e.originalEvent.key === 'ArrowUp' ||
+      e.originalEvent.key === 'ArrowLeft' ||
+      e.originalEvent.key === 'ArrowRight') tankMove(e.originalEvent.key);
   }
 
-  $(window).keydown(function(event) {
-    console.log(event);
-  });
+  $(window).keydown(keyIdentifier);
 });
+
+
+
+
+
+
+
+
 
 
 
@@ -100,15 +118,23 @@ $(() => {
 ///////- CODE GRAVEYARD -///////////
 ////////////////////////////////////
 
-// interval timer to change x/y of a selected div
+// //interval timer to change x/y of a selected div
 // function startInterval() {
 //   const intervalId = setInterval(changeXy, 1000);
 //   setTimeout(() => {
 //     clearInterval(intervalId);
 //   }, 10000);
 // }
-//function thats run in above interval
+// //function thats run in above interval
 // function changeXy () {
 //   $bullet.offset({left: $bullet.offset().left + 50 });
 //   printXy();
+// }
+
+// //prints position of boxes saved in object
+// function printXy () {
+//   console.log('tank minX: ' + tankPosition.minX);
+//   console.log('tank minY: ' + tankPosition.minY);
+//   console.log('bullet minX: ' + bullet.minX);
+//   console.log('bullet minY: ' + bullet.minY);
 // }
---
+++ pseudocode.js
@@ -0,0 +1,26 @@
+///////////////////////////////////////
+///////- COLLISION DETECTION -/////////
+///////////////////////////////////////
+
+///////- DIV MOVE AND X/Y LOG -////////
+//  log the position of a div   [X]
+//  -->save the xy of a div to a variable [X]
+//  -->determine the current position of a div x/y [log it to the console] [X]
+//  -->move the div and relog the position to the console [X]
+//----
+//  move the div on its own [X]
+//  -->continually log the xy to the console when moving [X]
+//____________________________________
+// const ;
+
+
+
+
+///////- COMPARE DIV 'A' TO 'B' -//////////
+//  determine when one div is within another [boolean response] [X]
+//  grab x/y from div A and B log to console and save as viables. [X]
+//  compare position of a to position of b [X]
+//  --> if A < B then log to console [X]
+
+
+//  move a div across the screen by updating its x and y position [jquery? css? js?]
