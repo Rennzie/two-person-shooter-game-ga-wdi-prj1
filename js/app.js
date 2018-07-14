7c3bbbd59f5d141b9efbb3d266a7c8e0956fcf4e
Sean.Rennie <sean.rennie6@gmail.com>
2018-07-14 19:15:42 +0100

    changed constructors to es6 classes to figure out inheritance

--- js/app.js
+++ js/app.js
@@ -10,8 +10,8 @@
 //  --> health [X]
 //  --> move independantly with different keys [X]
 //  --> update code to build tanks from a constructor [o]
-//    --> add one tank to screen feeding it starting coordinates [o]
-//    --> Move the tank using arrow keys [o]
+//    --> add one tank to screen feeding it starting coordinates [X]
+//    --> Move the tank using arrow keys [X]
 //    --> Fire bullets from the tank [o]
 //    --> add a second tank onto the screen [o]
 //    --> move the second tank idependantly [o]
@@ -19,8 +19,7 @@
 
 $(() => {
   const $battleField = $('.battle-field');
-  //const $tank = $('.tank');
-  const $tank2 = $('.tank2');
+  const $tank = $('.battle-field');
 
   const battleField = {
     left: $battleField.offset().left,
@@ -32,18 +31,19 @@ $(() => {
   ///////////////- TANK CONSTRUCTOR -////////////////////////
   ///////////////////////////////////////////////////////////
 
-  function Tank(startTop, startLeft) {
-    this.health = 100;
+  class Tank{
+    constructor (startTop, startLeft) {
+      this.health = 100;
 
-    this.dimensions = {
-      width: 50,
-      height: 50
-    };
+      this.dimensions = {
+        width: 50,
+        height: 50
+      };
 
-    this.element = document.createElement('div');
-    this.element.classList.add('tank');
+      this.element = document.createElement('div');
+      this.element.classList.add('tank');
 
-    this.element.style.cssText = `
+      this.element.style.cssText = `
       box-sizing: border-box;
       background-color: green;
       border: 1px solid brown;
@@ -53,14 +53,15 @@ $(() => {
       width: ${this.dimensions.width}px;
       height: ${this.dimensions.height}px;`;
 
-    this.currentPosition = {
-      left: startLeft,
-      top: startTop,
-      right: startLeft + this.dimensions.width,
-      bottom: startTop + this.dimensions.health
-    };
+      this.currentPosition = {
+        left: startLeft,
+        top: startTop,
+        right: startLeft + this.dimensions.width,
+        bottom: startTop + this.dimensions.health
+      };
 
-    this.movementPoints = 25;
+      this.movementPoints = 25;
+    }
   }
 
   Tank.prototype.addTank = function (){
@@ -72,7 +73,6 @@ $(() => {
     this.currentPosition.top = $(this.element).offset().top;
     this.currentPosition.right = $(this.element).offset().left + this.dimensions.width;
     this.currentPosition.bottom = $(this.element).offset().top + this.dimensions.height;
-    //this.detectCollision();
   };
 
   Tank.prototype.moveTank = function (direction){
@@ -106,168 +106,50 @@ $(() => {
       $(this.element).offset({left: $(this.element).offset().left + this.movementPoints});
   };
 
-  const playerOne = new Tank(battleField.top, battleField.left);
-
-  function addPlayerOne() {
-    console.log(battleField.top, battleField.left);
-    playerOne.addTank();
-  }
-
-  addPlayerOne();
+  //Tank.prototype.bullet = new Bullet();
 
 
 
-
-  /////////- TANK ONE -//////////////
-  // const tankOne = {
-  //   health: 100,
-  //
-  //   currentDirection: {
-  //     up: false,
-  //     down: false,
-  //     left: false,
-  //     right: false
-  //   },
-  //
-  //   currentPosition: {
-  //     left: $tank.offset().left,
-  //     top: $tank.offset().top,
-  //     right: $tank.offset().left + $tank.width(),
-  //     bottom: $tank.offset().top + $tank.height()
-  //   },
-  //
-  //   movementPoints: 50
-  // };
-
-  /////////- TANK TWO -//////////////
-  // const tankTwo = {
-  //   health: 100,
-  //
-  //   currentDirection: {
-  //     up: false,
-  //     down: false,
-  //     left: false,
-  //     right: false
-  //   },
-  //
-  //   currentPosition: {
-  //     left: $tank2.offset().left,
-  //     top: $tank2.offset().top,
-  //     right: $tank2.offset().left + $tank.width(),
-  //     bottom: $tank2.offset().top + $tank.height()
-  //   },
-  //
-  //   movementPoints: 50
-  // };
-
-
-  // ///////- MOVE TANK -//////////
-  // function tankMove(direction) {
-  //   updatedPosition(tankOne, $tank);
-  //   updatedPosition(tankTwo, $tank2);
-  //   switch(direction){
-  //     case 'ArrowUp':
-  //       return moveTankUp();
-  //     case 'ArrowDown':
-  //       return moveTankDown();
-  //     case 'ArrowLeft':
-  //       return moveTankLeft();
-  //     case 'ArrowRight':
-  //       return moveTankRight();
-  //     case 'w':
-  //       return moveTank2Up();
-  //     case 's':
-  //       return moveTank2Down();
-  //     case 'a':
-  //       return moveTank2Left();
-  //     case 'd':
-  //       return moveTank2Right();
-  //   }
-  // }
-  //
-  // function moveTankUp () {
-  //   if(tankOne.currentPosition.top - 10 > battleField.top)
-  //     $tank.offset({top: $tank.offset().top - tankOne.movementPoints});
-  // }
-  // function moveTankDown () {
-  //   if(tankOne.currentPosition.bottom < battleField.bottom)
-  //     $tank.offset({top: $tank.offset().top + tankOne.movementPoints});
-  // }
-  // function moveTankLeft () {
-  //   if(tankOne.currentPosition.left > battleField.left)
-  //     $tank.offset({left: $tank.offset().left - tankOne.movementPoints});
-  // }
-  // function moveTankRight () {
-  //   if(tankOne.currentPosition.right < battleField.right)
-  //     $tank.offset({left: $tank.offset().left + tankOne.movementPoints});
-  // }
-  //
-  // function moveTank2Up () {
-  //   if(tankTwo.currentPosition.top - 10 > battleField.top)
-  //     $tank2.offset({top: $tank2.offset().top - tankTwo.movementPoints});
-  // }
-  // function moveTank2Down () {
-  //   if(tankTwo.currentPosition.bottom < battleField.bottom)
-  //     $tank2.offset({top: $tank2.offset().top + tankTwo.movementPoints});
-  // }
-  // function moveTank2Left () {
-  //   if(tankTwo.currentPosition.left > battleField.left)
-  //     $tank2.offset({left: $tank2.offset().left - tankTwo.movementPoints});
-  // }
-  // function moveTank2Right () {
-  //   if(tankTwo.currentPosition.right < battleField.right)
-  //     $tank2.offset({left: $tank2.offset().left + tankTwo.movementPoints});
-  // }
-
-
-
-  ///////- ELEMENT POSITION UPDATER -/////////
-  //to dynamically update the position of an animated element
-  //(works when called as step in animateBullet())
-  function updatedPosition(itemObj, itemDom){
-    itemObj.currentPosition.left = itemDom.offset().left;
-    itemObj.currentPosition.top = itemDom.offset().top;
-    itemObj.currentPosition.right = itemDom.offset().left + itemDom.width();
-    itemObj.currentPosition.bottom = itemDom.offset().top + itemDom.height();
-    //targetCollision();
-  }
 
   ///////////////- BULLET CONSTRUCTOR -////////////////////////
   /////////////////////////////////////////////////////////////
-  function Bullet() {
-    this.placementPosition = {
-      left: tankOne.currentPosition.left + 50,
-      top: tankOne.currentPosition.top + 25
-    };
+  class Bullet extends Tank{
+    constructor () {
+      super();
+      this.placementPosition = {
+        left: this.currentPosition.left + 50,
+        top: this.currentPosition.top + 25
+      };
 
-    this.style = {
-      width: 25,
-      height: 10
-    };
+      this.style = {
+        width: 25,
+        height: 10
+      };
 
-    this.element = document.createElement('div');
-    this.element.classList.add('bullet');
+      this.element = document.createElement('div');
+      this.element.classList.add('bullet');
 
-    this.element.style.cssText = `
-      margin: 2px;
-      box-sizing: border-box;
-      border: 1px solid black;
-      position: absolute;
-      top: ${this.placementPosition.top}px;
-      left: ${this.placementPosition.left}px;
-      width: ${this.style.width}px;
-      height: ${this.style.height}px;`;
+      this.element.style.cssText = `
+        margin: 2px;
+        box-sizing: border-box;
+        border: 1px solid black;
+        position: absolute;
+        top: ${this.placementPosition.top}px;
+        left: ${this.placementPosition.left}px;
+        width: ${this.style.width}px;
+        height: ${this.style.height}px;`;
 
-    this.currentPosition = {
-      left: this.placementPosition.left,
-      top: this.placementPosition.top,
-      right: this.placementPosition.left + this.style.width,
-      bottom: this.placementPosition.top + this.style.height
-    };
+      this.currentPosition = {
+        left: this.placementPosition.left,
+        top: this.placementPosition.top,
+        right: this.placementPosition.left + this.style.width,
+        bottom: this.placementPosition.top + this.style.height
+      };
 
-    this.bulletSpeed = 5;
+      this.bulletSpeed = 5;
 
-    this.collisionDetected = false;
+      this.collisionDetected = false;
+    }
   }
 
   //refreshes the current xy coords of a bullet
@@ -289,7 +171,7 @@ $(() => {
 
   //repeatedly moves a bullet accross the screen
   Bullet.prototype.fireBullet = function() {
-    updatedPosition(tankOne, $tank);
+    //this.Tank.updatePosition();
     this.updatePosition();
     $(this.element).offset({left: $(this.element).offset().left + 10});
     this.updatePosition();
@@ -322,14 +204,21 @@ $(() => {
   //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
   ///////////////- BULLET CONSTRUCTOR -////////////////////////
 
+  const playerOne = new Tank(battleField.top, battleField.left);
+
+  function addPlayerOne() {
+    console.log(battleField.top, battleField.left);
+    playerOne.addTank();
+  }
+
 
   //to instantiate a new bullet and fire it across the screen
   function createBullet() {
-    const bullet = new Bullet;
-    bullet.addBullet();
-    bullet.fireBullet();
+    playerOne.addBullet();
+    playerOne.fireBullet();
   }
 
+  addPlayerOne();
 
   //////-KEY DOWN IDENTIFIER -///////
   //use this to determine what key has been pressed and assign correct function
@@ -382,4 +271,123 @@ $(() => {
 //   console.log('tank minY: ' + tankPosition.minY);
 //   console.log('bullet minX: ' + bullet.minX);
 //   console.log('bullet minY: ' + bullet.minY);
+// }
+
+
+
+
+///- OLD OBJECT FOR HARD CODED TANKS -///////////
+/////////////////////////////////////////////////
+/////////- TANK ONE -//////////////
+// const tankOne = {
+//   health: 100,
+//
+//   currentDirection: {
+//     up: false,
+//     down: false,
+//     left: false,
+//     right: false
+//   },
+//
+//   currentPosition: {
+//     left: $tank.offset().left,
+//     top: $tank.offset().top,
+//     right: $tank.offset().left + $tank.width(),
+//     bottom: $tank.offset().top + $tank.height()
+//   },
+//
+//   movementPoints: 50
+// };
+
+/////////- TANK TWO -//////////////
+// const tankTwo = {
+//   health: 100,
+//
+//   currentDirection: {
+//     up: false,
+//     down: false,
+//     left: false,
+//     right: false
+//   },
+//
+//   currentPosition: {
+//     left: $tank2.offset().left,
+//     top: $tank2.offset().top,
+//     right: $tank2.offset().left + $tank.width(),
+//     bottom: $tank2.offset().top + $tank.height()
+//   },
+//
+//   movementPoints: 50
+// };
+
+
+// ///////- MOVE TANK -//////////
+// function tankMove(direction) {
+//   updatedPosition(tankOne, $tank);
+//   updatedPosition(tankTwo, $tank2);
+//   switch(direction){
+//     case 'ArrowUp':
+//       return moveTankUp();
+//     case 'ArrowDown':
+//       return moveTankDown();
+//     case 'ArrowLeft':
+//       return moveTankLeft();
+//     case 'ArrowRight':
+//       return moveTankRight();
+//     case 'w':
+//       return moveTank2Up();
+//     case 's':
+//       return moveTank2Down();
+//     case 'a':
+//       return moveTank2Left();
+//     case 'd':
+//       return moveTank2Right();
+//   }
+// }
+//
+// function moveTankUp () {
+//   if(tankOne.currentPosition.top - 10 > battleField.top)
+//     $tank.offset({top: $tank.offset().top - tankOne.movementPoints});
+// }
+// function moveTankDown () {
+//   if(tankOne.currentPosition.bottom < battleField.bottom)
+//     $tank.offset({top: $tank.offset().top + tankOne.movementPoints});
+// }
+// function moveTankLeft () {
+//   if(tankOne.currentPosition.left > battleField.left)
+//     $tank.offset({left: $tank.offset().left - tankOne.movementPoints});
+// }
+// function moveTankRight () {
+//   if(tankOne.currentPosition.right < battleField.right)
+//     $tank.offset({left: $tank.offset().left + tankOne.movementPoints});
+// }
+//
+// function moveTank2Up () {
+//   if(tankTwo.currentPosition.top - 10 > battleField.top)
+//     $tank2.offset({top: $tank2.offset().top - tankTwo.movementPoints});
+// }
+// function moveTank2Down () {
+//   if(tankTwo.currentPosition.bottom < battleField.bottom)
+//     $tank2.offset({top: $tank2.offset().top + tankTwo.movementPoints});
+// }
+// function moveTank2Left () {
+//   if(tankTwo.currentPosition.left > battleField.left)
+//     $tank2.offset({left: $tank2.offset().left - tankTwo.movementPoints});
+// }
+// function moveTank2Right () {
+//   if(tankTwo.currentPosition.right < battleField.right)
+//     $tank2.offset({left: $tank2.offset().left + tankTwo.movementPoints});
+// }
+
+
+
+// ///////- OLD ELEMENT POSITION UPDATER BEFORE TANK CONSTRUCTOR -/////////
+// //to dynamically update the position of an animated element
+// //(works when called as step in animateBullet())
+// function updatedPosition(itemObj, itemDom){
+//   itemObj.currentPosition.left = itemDom.offset().left;
+//   itemObj.currentPosition.top = itemDom.offset().top;
+//   itemObj.currentPosition.right = itemDom.offset().left + itemDom.width();
+//   itemObj.currentPosition.bottom = itemDom.offset().top + itemDom.height();
+//   //targetCollision();
 // }
