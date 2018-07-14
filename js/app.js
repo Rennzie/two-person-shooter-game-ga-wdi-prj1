87bdd6c01d49595d1da53918d20f77733ba438a2
Sean.Rennie <sean.rennie6@gmail.com>
2018-07-14 14:50:09 +0100

    added in a bullet by calling a constructor

--- js/app.js
+++ js/app.js
@@ -1,32 +1,10 @@
 //console.log('this is linked');
-////////////////////////////////////
-///////- TANK MOVE -//////////
-////////////////////////////////////
-//  make the tanks position relative [X]
-//  grab tank element from the dom [X]
-//  update the x position few pixels on up key press [X]
-//  limit the movement of the tank to its container [o]
-//  --> before moving tank, check its not withi the board
-//  --> update to position of the tank after every keypress
-//  --> this should stop the tank form moving in that direction
-
-
-// --> update position of tank at start of the on move function
-//
-// --> on append of bullet, start an interval which runs update position function every millisecond
-// --> interval runs function which check bullets position against position of opponent tank
-// --> Interval will run while bullet is with battle field parameters of until it hits a tank
-// --> once it breaks the while loop, bullet is removed if at edge of battle field or explode if hits target
-
-
-
 
 ////////////////////////////////////
 ///////- DOM INTERACTION -//////////
 ////////////////////////////////////
 
 $(() => {
-  const $bullet = $('.bullet');
   const $tank = $('.tank');
   const $battleField = $('.battle-field');
 
@@ -38,7 +16,7 @@ $(() => {
   };
 
   const tankOne = {
-    position: {
+    currentPosition: {
       left: $tank.offset().left,
       top: $tank.offset().top,
       right: $tank.offset().left + $tank.width(),
@@ -46,7 +24,6 @@ $(() => {
     },
 
     movementPoints: 50
-
   };
 
   ///////- MOVE TANK -//////////
@@ -65,67 +42,138 @@ $(() => {
   }
 
   function moveTankUp () {
-    if(tankOne.position.top - 10 > battleField.top)
+    if(tankOne.currentPosition.top - 10 > battleField.top)
       $tank.offset({top: $tank.offset().top - tankOne.movementPoints});
   }
   function moveTankDown () {
-    if(tankOne.position.bottom < battleField.bottom)
+    if(tankOne.currentPosition.bottom < battleField.bottom)
       $tank.offset({top: $tank.offset().top + tankOne.movementPoints});
   }
   function moveTankLeft () {
-    if(tankOne.position.left > battleField.left)
+    if(tankOne.currentPosition.left > battleField.left)
       $tank.offset({left: $tank.offset().left - tankOne.movementPoints});
   }
   function moveTankRight () {
-    if(tankOne.position.right < battleField.right)
+    if(tankOne.currentPosition.right < battleField.right)
       $tank.offset({left: $tank.offset().left + tankOne.movementPoints});
   }
 
 
-  const bullet = {
-    left: $bullet.offset().left,
-    top: $bullet.offset().top,
-    right: $bullet.offset().left + $bullet.width(),
-    bottom: $bullet.offset().top + $bullet.height(),
-    collisionDetected: false
-  };
 
-
-  ///////- COLLISION DETECTION -/////////
-
+  ///////- ELEMENT POSITION UPDATER -/////////
   //to dynamically update the position of an animated element
   //(works when called as step in animateBullet())
   function updatedPosition(itemObj, itemDom){
-    itemObj.position.left = itemDom.offset().left;
-    itemObj.position.top = itemDom.offset().top;
-    itemObj.position.right = itemDom.offset().left + itemDom.width();
-    itemObj.position.bottom = itemDom.offset().top + itemDom.height();
-    targetCollision();
+    itemObj.currentPosition.left = itemDom.offset().left;
+    itemObj.currentPosition.top = itemDom.offset().top;
+    itemObj.currentPosition.right = itemDom.offset().left + itemDom.width();
+    itemObj.currentPosition.bottom = itemDom.offset().top + itemDom.height();
+    //targetCollision();
+  }
+  ///////- COLLISION DETECTION -/////////
+  // function targetCollision () {
+  //   if(bullet.right > tankOne.left){
+  //     bullet.collisionDetected = true;
+  //     console.log('HIT HIT HIT ' + bullet.collisionDetected);
+  //   }
+  // }
+
+  ////////////////////////////////////
+  ///////- APPEND BULLET AND REMOVE BULLET FROM BOARD -//////////
+  ////////////////////////////////////
+  // --> on append of bullet, start an interval which runs update position function every millisecond
+  // --> interval runs function which check bullets position against position of opponent tank
+  // --> Interval will run while bullet is with battle field parameters of until it hits a tank
+  // --> once it breaks the while loop, bullet is removed if at edge of battle field or explode if hits target
+  //Append bullet to screen
+
+
+  function Bullet() {
+    this.element = document.createElement('div');
+    this.element.classList.add('bullet');
+
+    this.placementPosition = {
+      left: tankOne.currentPosition.left,
+      top: tankOne.currentPosition.top
+    };
+
+    this.style = {
+      width: 25,
+      height: 10
+    };
+
+    this.currentPosition = {
+      left: this.placementPosition.left,
+      top: this.placementPosition.top,
+      right: this.placementPosition.left + this.style.width,
+      bottom: this.placementPosition.top + this.style.height
+    };
+
+    this.collisionDetected = false;
   }
 
-  function targetCollision () {
-    if(bullet.right > tankOne.left){
-      bullet.collisionDetected = true;
-      console.log('HIT HIT HIT ' + bullet.collisionDetected);
-    }
+  Bullet.prototype.updatePosition = function (){
+    this.currentPosition.left = $(this.element).offset().left;
+    this.currentPosition.top = $(this.element).offset().top;
+    this.currentPosition.right = $(this.element).offset().left + this.style.width;
+    this.currentPosition.bottom = $(this.element).offset().top + this.style.height;
+    //targetCollision();
+  };
+
+  Bullet.prototype.addBullet = function (){
+    $battleField.append(this.element);
+    //fireBullet();
+  };
+
+  //grab bullet from DOM.
+  //update its object POSITION
+  //move it along x axis at 10px per 10ms
+  //do this while its still within the board
+
+  Bullet.prototype.fireBullet = function() {
+    console.log(this);
+    this.updatePosition();
+    console.log(this);
+    //const $bullet = $('.bullet');
+    //updatedPosition(Bullet, this);
+
+    // if(Bullet.currentPosition.left > battleField.left &&
+    // Bullet.currentPosition.right < battleField.right &&
+    // Bullet.currentPosition.top > battleField.top &&
+    // Bullet.currentPosition.bottom < battleField.bottom ){
+
+      // $bullet.offset({left: $bullet.offset().left + 10});
+      // updatedPosition(Bullet, $bullet);
+      // setTimeout( () => {
+      //   fireBullet();
+      // },10);
+    // }else {
+    //   return;
+    // }
+
+
+    // $bullet.animate({
+    //   left: '100%'
+    // }, {
+    //   duration: 1000
+    //   //step: updatedPosition
+    // });
   }
 
   //to move bullet around screen and update its position object
-  function animateBullet() {
-    console.log('I fired');
-    $bullet.animate({
-      left: '100%'
-    }, {
-      duration: 1000,
-      step: updatedPosition
-    });
+  function createBullet() {
+    const bullet = new Bullet;
+    bullet.addBullet();
+    bullet.fireBullet();
+
   }
+
 
   //////-KEY DOWN IDENTIFIER -///////
   //use this to determine what key has been pressed and assign correct function
   function keyIdentifier(e){
     //console.log(e);
-    if(e.originalEvent.key === ' ') animateBullet();
+    if(e.originalEvent.key === ' ') createBullet();
     if(e.originalEvent.key === 'ArrowDown' ||
       e.originalEvent.key === 'ArrowUp' ||
       e.originalEvent.key === 'ArrowLeft' ||
--- pseudocode.js
+++ pseudocode.js
@@ -24,3 +24,16 @@
 
 
 //  move a div across the screen by updating its x and y position [jquery? css? js?]
+
+
+////////////////////////////////////
+///////- TANK MOVE -//////////
+////////////////////////////////////
+//  make the tanks position relative [X]
+//  grab tank element from the dom [X]
+//  update the x position few pixels on up key press [X]
+//  limit the movement of the tank to its container [X]
+//  --> before moving tank, check its not withi the board [X]
+//  --> update to position of the tank after every keypress [X]
+//  --> this should stop the tank form moving in that direction [X]
+// --> update position of tank at start of the on move function [X]
